b954f70c38af9f0c2c8f2287c6406adc93d8eb6e
zjun <565061763@qq.com>
2021-04-19 09:58:39 +0800

    FDialogConfirmView

--- lib/src/dialog_confirm_view.dart
+++ lib/src/dialog_confirm_view.dart
@@ -6,10 +6,10 @@ import 'dialog_alert_view.dart';
 import 'locale.dart';
 
 class FDialogConfirmView extends StatelessWidget with FDialogViewMixin {
-  final Widget title;
-  final Widget content;
-  final FDialogAction cancel;
-  final FDialogAction confirm;
+  final Widget? title;
+  final Widget? content;
+  final FDialogAction? cancel;
+  final FDialogAction? confirm;
 
   FDialogConfirmView({
     this.title,
@@ -19,19 +19,17 @@ class FDialogConfirmView extends StatelessWidget with FDialogViewMixin {
   });
 
   factory FDialogConfirmView.simple({
-    String title = '',
-    Widget content,
-    String cancel = '',
-    VoidCallback onClickCancel,
-    String confirm = '',
-    VoidCallback onClickConfirm,
-    @required BuildContext context,
+    String? title = '',
+    Widget? content,
+    String? cancel = '',
+    VoidCallback? onClickCancel,
+    String? confirm = '',
+    VoidCallback? onClickConfirm,
+    required BuildContext context,
   }) {
-    Widget widgetTitle;
+    Widget? widgetTitle;
     if (title != null) {
-      if (title == '') {
-        title = FLibDialogLocale.tips(context);
-      }
+      if (title == '') title = FLibDialogLocale.tips(context);
 
       widgetTitle = Text(
         title,
@@ -40,13 +38,11 @@ class FDialogConfirmView extends StatelessWidget with FDialogViewMixin {
       );
     }
 
-    Widget widgetContent = content;
+    Widget? widgetContent = content;
 
-    FDialogAction widgetCancel;
+    FDialogAction? widgetCancel;
     if (cancel != null) {
-      if (cancel == '') {
-        cancel = FLibDialogLocale.cancel(context);
-      }
+      if (cancel == '') cancel = FLibDialogLocale.cancel(context);
 
       widgetCancel = FDialogAction(
         child: Text(
@@ -55,20 +51,12 @@ class FDialogConfirmView extends StatelessWidget with FDialogViewMixin {
           overflow: TextOverflow.ellipsis,
         ),
         onClick: onClickCancel,
-        borderRadius: confirm == null
-            ? BorderRadius.only(
-                bottomLeft: Radius.circular(5.0),
-                bottomRight: Radius.circular(5.0),
-              )
-            : BorderRadius.only(bottomLeft: Radius.circular(5.0)),
       );
     }
 
-    FDialogAction widgetConfirm;
+    FDialogAction? widgetConfirm;
     if (confirm != null) {
-      if (confirm == '') {
-        confirm = FLibDialogLocale.confirm(context);
-      }
+      if (confirm == '') confirm = FLibDialogLocale.confirm(context);
 
       widgetConfirm = FDialogAction(
         child: Text(
@@ -77,12 +65,6 @@ class FDialogConfirmView extends StatelessWidget with FDialogViewMixin {
           overflow: TextOverflow.ellipsis,
         ),
         onClick: onClickConfirm,
-        borderRadius: cancel == null
-            ? BorderRadius.only(
-                bottomLeft: Radius.circular(5.0),
-                bottomRight: Radius.circular(5.0),
-              )
-            : BorderRadius.only(bottomRight: Radius.circular(5.0)),
       );
     }
 
@@ -99,11 +81,11 @@ class FDialogConfirmView extends StatelessWidget with FDialogViewMixin {
     final List<Widget> listAction = [];
 
     if (cancel != null) {
-      listAction.add(cancel);
+      listAction.add(cancel!);
     }
 
     if (confirm != null) {
-      listAction.add(confirm);
+      listAction.add(confirm!);
     }
 
     return FDialogAlertView(
